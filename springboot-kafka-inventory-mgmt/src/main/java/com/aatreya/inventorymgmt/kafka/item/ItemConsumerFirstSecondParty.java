c3618014d3922cb49683df3df6a1f7e7ea9211b7
aputtapa000 <aputtapa000@gmail.com>
2026-01-23 17:03:20 -0800

    Previous typo for item consumer first second party class now corrected

--- springboot-kafka-inventory-mgmt/src/main/java/com/aatreya/inventorymgmt/kafka/item/ItemConsumerFirstSecondParty.java
+++ springboot-kafka-inventory-mgmt/src/main/java/com/aatreya/inventorymgmt/kafka/item/ItemConsumerFirstSecondParty.java
@@ -4,9 +4,9 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class InventoryConsumerFirstSecondParty {
+public class ItemConsumerFirstSecondParty {
 
-    private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(InventoryConsumerFirstSecondParty.class);
+    private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(ItemConsumerFirstSecondParty.class);
 
     @KafkaListener(topics = "item-first-second-party-topic", groupId = "item-first-second-party-group")
     public void consumer(String message) {
