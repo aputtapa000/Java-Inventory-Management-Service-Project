930b7ce3bd815afbd9d9f96a338b9db7b48b8aa4
aputtapa000 <aputtapa000@gmail.com>
2026-02-13 17:25:19 -0800

    feat: update ItemConsumer classes to handle Item objects and save to Cosmos DB with validation

--- springboot-kafka-inventory-mgmt/src/main/java/com/aatreya/inventorymgmt/kafka/item/ItemConsumerDSV.java
+++ springboot-kafka-inventory-mgmt/src/main/java/com/aatreya/inventorymgmt/kafka/item/ItemConsumerDSV.java
@@ -1,15 +1,41 @@
 package com.aatreya.inventorymgmt.kafka.item;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import com.aatreya.inventorymgmt.repository.ItemRepository;
+import com.aatreya.inventorymgmt.model.Item;
 
 @Service
+@ConditionalOnProperty(prefix = "spring.cloud.azure.cosmos", name = "enabled", havingValue = "true")
 public class ItemConsumerDSV {
 
     private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(ItemConsumerDSV.class);
+    private final ItemRepository itemRepository;
+
+    public ItemConsumerDSV(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
 
     @KafkaListener(topics = "item-DSV-topic", groupId = "item-DSV-group")
-    public void consumer(String message) {
-        LOGGER.info(String.format("Message received -> %s", message));
+        public void consumer(Item item) {
+        try{
+        LOGGER.info(String.format("Message received -> %s", item.toString()));
+
+            // Validate required fields
+            if (item.getSku_id() == 0) {
+                LOGGER.error("SKU ID cannot be null or empty");
+                return;
+            }
+            if (item.getCategory() == null || item.getCategory().isEmpty()) {
+                LOGGER.error("Category (partition key) cannot be null or empty");
+                return;
+            }
+            itemRepository.save(item);
+            LOGGER.info(String.format("Item saved to Cosmos DB with SKU ID: %s", item.getSku_id()));
+
+        } catch (Exception e) {
+            LOGGER.error(String.format("Error processing item message: %s", e.getMessage()), e);
+        }
     }
-}
+}   
